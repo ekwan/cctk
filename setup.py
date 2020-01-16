bc3242b0cd013e51321c7867ef9fc291472a612e
Corin Wagen <corin.wagen@gmail.com>
2020-01-16 06:58:20 -0500

    hoping to add to pypi

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name="cctk",
+    packages=["cctk"],
+    version="v0.1.0",
+    license="Apache 2.O",
+    description="computational chemistry toolkit",
+    author="Corin Wagen and Eugene Kwan",
+    author_email="corin.wagen@gmail.com",
+    url="https://github.com/ekwan/cctk",
+    download_url="https://github.com/ekwan/cctk/archive/v0.1.0.tar.gz",
+    install_requires=["numpy", "networkx",],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache License 2.0",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
