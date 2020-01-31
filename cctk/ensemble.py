fixed a bug pertaining to rotation and alignment in ensembles, pushing hotfix now

cctk/ensemble.py
@@ -217,17 +217,27 @@ class ConformationalEnsemble(Ensemble):
         Args:
             align_to (int): which geometry to align to (1-indexed)
             atoms (list): which atoms to align in each molecule (1-indexed; must be at least 3)
+                alternatively, specify "None" for all atoms or "heavy" for all heavy atoms
+
+        Returns:
+            a new Ensemble() object with the objects aligned
         """
         self._check_molecule_number(align_to)
 
-        if atoms and (len(atoms) < 3):
-            raise ValueError("not enough atoms for alignment - need 3 in 3D space!")
+        if atoms is None:
+            atoms = np.arange(0, self.molecules[0].num_atoms())
+        elif isinstance(atoms, str) and (atoms == "heavy"):
+            atoms = self.molecules[0].get_heavy_atoms()
+        else:
+            try:
+                atoms = np.array(atoms)
+                atoms += -1
 
-        try:
-            atoms = np.array(atoms)
-            atoms += -1
-        except:
-            raise ValueError("atom_numbers cannot be cast to numpy array... disappointing!")
+                if len(atoms) < 3:
+                    raise ValueError("not enough atoms for alignment - need 3 in 3D space!")
+
+            except:
+                raise ValueError("atom numbers is not a recognized keyword and cannot be cast to numpy array... try again!")
 
         #### atom numbers is 0-indexed now
         #### move everything to the center!
@@ -237,10 +247,10 @@ class ConformationalEnsemble(Ensemble):
 
         template = self.molecules[align_to - 1].geometry[atoms]
 
-        #### perform alignment
+        #### perform alignment using Kabsch algorithm
         for molecule in self.molecules:
             new_geometry = align_matrices(molecule.geometry[atoms], molecule.geometry, template)
-            self.geometry = new_geometry
+            molecule.geometry = new_geometry
 
             assert len(molecule.geometry) == len(molecule.atomic_numbers), "wrong number of geometry elements!"
 

cctk/molecule.py
@@ -607,7 +607,7 @@ class Molecule:
         rot_matrix = compute_rotation_matrix(axis, degrees)
 
         for atom in range(0, self.num_atoms()):
-            self.geometry[atom] = list(np.dot(rot_matrix, self.get_vector(atom)))
+            self.geometry[atom] = list(np.dot(rot_matrix, self.geometry[atom]))
 
         return self
 

test/core_tests.py
@@ -1,4 +1,4 @@
-import unittest, sys, os, io
+import unittest, sys, os, io, copy
 import numpy as np
 import cctk
 
@@ -244,6 +244,46 @@ class TestXYZ(unittest.TestCase):
                 )
 
         os.remove(new_path)
+
+class TestEnsemble(unittest.TestCase):
+    def generate_test_ensemble(self):
+        path = "static/test_peptide.xyz"
+        file = cctk.XYZFile.read_file(path)
+        mol = file.molecule
+
+        e1 = np.array([1, 0, 0])
+        e2 = np.array([0, 1, 0])
+        e3 = np.array([0, 0, 1])
+
+        ensemble = cctk.ConformationalEnsemble()
+        ensemble.add_molecule(mol)
+        self.assertEqual(len(ensemble.molecules), 1)
+
+        mol_rot = copy.deepcopy(ensemble.molecules[0]).rotate_molecule(e1, 90)
+        ensemble.add_molecule(mol_rot)
+        self.assertEqual(len(ensemble.molecules), 2)
+
+        mol_trans = copy.deepcopy(ensemble.molecules[0]).translate_molecule(e2)
+        ensemble.add_molecule(mol_trans)
+        self.assertEqual(len(ensemble.molecules), 3)
+
+        mol_trans_rot = ensemble.molecules[1].translate_molecule(e2)
+        ensemble.add_molecule(mol_trans_rot)
+        self.assertEqual(len(ensemble.molecules), 4)
+
+        mol_rot_trans = ensemble.molecules[2].rotate_molecule(e1, 90)
+        ensemble.add_molecule(mol_rot_trans)
+        self.assertEqual(len(ensemble.molecules), 5)
+
+        return ensemble
+
+    def test_align(self):
+        ensemble = self.generate_test_ensemble()
+        ensemble.align()
+        template = ensemble.molecules[0].geometry
+        for molecule in ensemble.molecules:
+            for i in range(0,len(template)):
+                self.assertTrue(cctk.helper_functions.compute_distance_between(molecule.geometry[i],template[i]) < 0.0001)
 
 if __name__ == '__main__':
     unittest.main()