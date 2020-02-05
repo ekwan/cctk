9aa24206a29af56a30cdc92a2eac9f06c3c3d7e6
Corin Wagen <corin.wagen@gmail.com>
2020-02-04 20:51:18 -0500

    working on integrating with readthedocs, part ii

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,8 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-#import os
-import cctk 
-#sys.path.insert(0, os.path.relpath('../cctk'))
+import os, sys
+sys.path.insert(0, os.path.relpath('../cctk'))
 
 master_doc = 'index'
 
