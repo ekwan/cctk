eaa461e272ccd53973593e14ba691fc8f0cb4787
Eugene Kwan <ekwan16@gmail.com>
2020-04-06 11:13:36 -0400

    NMR tests fixed

--- test/test_nmr.py
+++ test/test_nmr.py
@@ -13,8 +13,12 @@ class TestNMR(unittest.TestCase):
         ensemble = gaussian_file.molecules
         molecule = ensemble[-1]
         properties = ensemble[molecule]
-        self.assertEqual(properties["energy"], -40.5169484082)
-        self.assertListEqual(list(properties["isotropic_shielding"]), [198.2259, 32.6869, 32.6869, 32.6869, 32.6869])
+        energy = properties["energy"]
+        self.assertEqual(energy, -40.5169484082)
+        shieldings = properties["isotropic_shielding"]
+        self.assertListEqual(list(shieldings), [198.2259, 32.6869, 32.6869, 32.6869, 32.6869])
+        shieldings = ensemble[:,"isotropic_shielding"]
+        self.assertListEqual(list(shieldings[0]), [198.2259, 32.6869, 32.6869, 32.6869, 32.6869])
 
     def test_nmr2(self):
         # this file contains opt freq followed by Link1 NMR on methane
@@ -23,15 +27,16 @@ class TestNMR(unittest.TestCase):
         first_link = gaussian_file[0]
         self.assertListEqual(first_link.job_types, [JobType.OPT, JobType.FREQ, JobType.SP])
         ensemble = first_link.molecules
-        energies = [ ensemble[molecule]["energy"] for molecule in ensemble.molecules() ]
+        #energies = [ ensemble[molecule]["energy"] for molecule in ensemble.molecules() ]
+        energies = list(ensemble[:,"energy"])
         self.assertListEqual(energies, [-40.5169484082, -40.5183831835, -40.5183831835])
         #for molecule,properties in ensemble:
         #    print(molecule)
         #    print(properties)
         second_link = gaussian_file[1]
         ensemble = second_link.molecules
-        last_molecule = ensemble[-1]
-        shifts = list(ensemble[last_molecule]["isotropic_shielding"])
+        #last_molecule = ensemble[-1]
+        shifts = list(ensemble[-1,"isotropic_shielding"])
         self.assertListEqual(shifts, [192.9242, 31.8851, 31.8851, 31.8851, 31.8851])
 
     def test_nmr3(self):
@@ -44,14 +49,16 @@ class TestNMR(unittest.TestCase):
         self.assertListEqual(second_link.job_types, [JobType.NMR, JobType.SP])
         ensemble = second_link.molecules
         molecule = ensemble[-1]
-        shifts = list(ensemble[molecule]["isotropic_shielding"])
-        self.assertListEqual(shifts, [180.3673, 31.2068, 31.207, 31.2068, 180.3673, 31.2068, 31.207, 31.2068])
+        #shifts = list(ensemble[molecule]["isotropic_shielding"])
+        shifts = ensemble[:,"isotropic_shielding"]
+        self.assertListEqual(list(shifts[0]), [180.3673, 31.2068, 31.207, 31.2068, 180.3673, 31.2068, 31.207, 31.2068])
         third_link = gaussian_file[2]
         self.assertListEqual(third_link.job_types, [JobType.NMR, JobType.SP])
         ensemble = third_link.molecules
-        molecule = ensemble[-1]
-        shifts = list(ensemble[molecule]["isotropic_shielding"])
-        self.assertListEqual(shifts, [198.2259, 32.6869, 32.6869, 32.6869, 32.6869])
+        #molecule = ensemble[-1]
+        #shifts = list(ensemble[molecule]["isotropic_shielding"])
+        shifts = ensemble[:,"isotropic_shielding"]
+        self.assertListEqual(list(shifts[0]), [198.2259, 32.6869, 32.6869, 32.6869, 32.6869])
 
 if __name__ == '__main__':
     unittest.main()
