8c469a24c024750811f831c675fb6db71a0733ee
Corin Wagen <corin.wagen@gmail.com>
2020-04-27 15:34:20 -0400

    made a nicer analyze.py still

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -1,7 +1,9 @@
 import sys, re, glob, cctk
 import numpy as np
 import pandas as pd
+
 from tabulate import tabulate
+from tqdm import tqdm
 
 #### This is a script to monitor the output of Gaussian files. 
 #### In contrast to ``monitor.py``, this script analyzes many files! 
@@ -17,8 +19,9 @@ from tabulate import tabulate
 filenames = sys.argv[1]
 results = cctk.Ensemble()
 
-for filename in sorted(glob.glob(filenames, recursive=True)):
-    print(f"reading file {filename}")
+print("\n\033[3mreading files:\033[0m")
+
+for filename in tqdm(sorted(glob.glob(filenames, recursive=True))):
     if re.search("slurm", filename):
         continue
 
@@ -44,12 +47,21 @@ if len(results) == 0:
     print("no jobs to analyze!")
     exit()
 
-property_names = ["filename", "iters", "energy", "enthalpy", "gibbs_free_energy", "rms_force", "rms_displacement", "success", "imaginary"]
+print("\n\n\033[3manalysis:\033[0m\n")
+property_names = ["filename", "iters", "energy", "enthalpy", "quasiharmonic_gibbs_free_energy", "rms_force", "rms_displacement", "success", "imaginary"]
 values = results[:, property_names]
 if not isinstance(values[0], list):
     values = [values]
 
-df = pd.DataFrame(values, columns=property_names)
+df = pd.DataFrame(values, columns=property_names).fillna("")
+df.rename(columns={"rms_displacement": "rms_disp", "quasiharmonic_gibbs_free_energy": "GFE (corrected)"}, inplace=True)
 df["rel_energy"] = (df.energy - df.energy.min()) * 627.509469
+
+df["rms_force"] = df['rms_force'].apply(lambda x: f"\033[92m{x}\033[0m" if float(x or 0) < 0.0001 else f"\033[93m{x}\033[0m")
+df["rms_disp"] = df['rms_disp'].apply(lambda x: f"\033[92m{x}\033[0m" if float(x or 0) < 0.003 else f"\033[93m{x}\033[0m")
+df["success"] = df['success'].apply(lambda x: f"\033[92m{x}\033[0m" if x else f"\033[93m{x}\033[0m")
+
+
+df.columns = [f"\033[1m{c}\033[0m" for c in df.columns]
 print(tabulate(df, headers="keys", tablefmt="presto", floatfmt=".5f"))
 
